be1c44cd335dd06015290e1ee59e10f4f66ee7f9
AnnaBland <+AnnaBland@users.noreply.github.com>
2020-04-18 14:30:34 +0300

    Added method for search distance between 2 points
    Lesson 1 - task 1

---
+++ sandbox/src/main/java/ru/stqa/pft/lessonOne/partOne/Main.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.lessonOne.partOne;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point p = new Point(1, 1,5, 5);
+        System.out.println(p.distance());}
+
+    }
