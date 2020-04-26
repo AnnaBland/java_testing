3e90e67c9a504190edba0a6fba378b8fac6a8059
AnnaBland <+AnnaBland@users.noreply.github.com>
2020-04-26 17:17:52 +0300

    Create HelperBase, optimize ContactHelper,GroupHelper,NavigationHelper,SessionHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -4,31 +4,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactHelper {
-    private WebDriver wd;
+public class ContactHelper extends HelperBase{
 
     public ContactHelper(WebDriver wd) {
-        this.wd = wd;
+        super (wd);
     }
 
     public void submitAddNewContact() {
-        wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+        click(By.xpath("(//input[@name='submit'])[2]"));
     }
 
     public void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-        wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-        wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
-        wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
-        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-        wd.findElement(By.name("home")).sendKeys(contactData.getHomephone());
-        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-        wd.findElement(By.name("work")).sendKeys(contactData.getWork());
-        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+        type(By.name("firstname"),contactData.getFirstname());
+        type(By.name("middlename"),contactData.getMiddlename());
+        type(By.name("lastname"),contactData.getLastname());
+        type(By.name("nickname"),contactData.getNickname());
+        type(By.name("company"),contactData.getCompany());
+        type(By.name("address"),contactData.getAddress());
+        type(By.name("home"),contactData.getHomephone());
+        type(By.name("mobile"),contactData.getMobile());
+        type(By.name("work"),contactData.getWork());
+        type(By.name("email"),contactData.getEmail());
     }
 
     public void AddNewContact() {
-        wd.findElement(By.linkText("add new")).click();
+        click (By.linkText("add new"));
     }
 }
