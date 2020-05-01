3b4f279e4ec9352a42ee27d99d23bcda9b139173
AnnaBland <+AnnaBland@users.noreply.github.com>
2020-05-01 12:52:49 +0300

    Replace "==" to "equals"

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -20,14 +20,19 @@ public class ApplicationManager {
     private String browser;
 
 
-    public ApplicationManager (String browser){
+    public ApplicationManager(String browser) {
         this.browser = browser;
     }
 
     public void init() {
-        if (browser == BrowserType.FIREFOX){wd = new FirefoxDriver();}
-        else if (browser == BrowserType.CHROME){wd = new ChromeDriver();}
-        else if (browser == BrowserType.IE){wd = new InternetExplorerDriver();}
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        }
+        else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
 
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
@@ -43,9 +48,10 @@ public class ApplicationManager {
         wd.quit();
     }
 
-    public  ContactHelper contactHelper(){
+    public ContactHelper contactHelper() {
         return contactHelper;
     }
+
     public GroupHelper getGroupHelper() {
         return groupHelper;
     }
