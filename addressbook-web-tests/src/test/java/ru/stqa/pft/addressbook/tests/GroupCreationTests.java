f8bb447369cc580f518babd1a1c8176f94aa0fc8
anna.blandinova <anna.blandinova@gs-labs.ru>
2020-05-26 11:40:35 +0300

    Logging

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
@@ -18,6 +20,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromXML() throws IOException {
@@ -58,6 +61,7 @@ public class GroupCreationTests extends TestBase {
     Groups after =app.group().all();
     assertThat(after, equalTo(
             before.withAdded( group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
 
   }
 
