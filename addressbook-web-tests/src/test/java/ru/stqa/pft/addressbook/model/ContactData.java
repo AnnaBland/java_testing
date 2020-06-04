9be402c0743a00f5d7e719423bae1405d92e59c1
anna.blandinova <anna.blandinova@gs-labs.ru>
2020-06-04 19:46:54 +0300

    Changed HbConnectionTest for ContactData

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,54 +3,84 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
 
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name="addressbook")
 public class ContactData {
     @XStreamOmitField
+    @Id
+    @Column(name = "id")
     private int id = Integer.MAX_VALUE;
     @Expose
+    @Column(name = "email")
+    @Type(type="text")
     private String email;
     @Expose
+    @Column(name = "email2")
+    @Type(type="text")
     private String email2;
     @Expose
+    @Column(name = "email3")
+    @Type(type="text")
     private String email3;
     @Expose
+    @Column(name = "firstname")
     private String firstname;
     @Expose
     private String middlename;
     @Expose
+    @Column(name = "lastname")
     private String lastname;
     @Expose
+    @Column(name = "nickname")
     private String nickname;
     @Expose
+    @Column(name = "company")
     private String company;
     @Expose
+    @Column(name = "address")
+    @Type(type="text")
     private String address;
     @Expose
+    @Column(name = "home")
+    @Type(type="text")
     private String homephone;
     @Expose
+    @Column(name = "mobile")
+    @Type(type="text")
     private String mobile;
     @Expose
+    @Column(name = "work")
+    @Type(type="text")
     private String work;
     @Expose
+    @Transient
     private String group;
     @Expose
+    @Transient
     private String allPhones;
     @Expose
+    @Transient
     private String allEmails;
     @XStreamOmitField
-    private File photo;
+    @Column(name = "photo")
+    @Type(type="text")
+    private String photo;
 
     public ContactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath();
         return this;
     }
 
     public File getPhoto () {
-        return photo;
+        return new File (photo);
     }
 
     public ContactData withId(int id) {
