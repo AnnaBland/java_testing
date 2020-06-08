78c48b94a9f9ca9cbfd6124aa0965de052affb52
anna.blandinova <anna.blandinova@gs-labs.ru>
2020-06-08 10:54:23 +0300

    Added method verifyGroupListInUI

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -7,10 +7,19 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
 public class TestBase {
     Logger logger = LoggerFactory.getLogger(TestBase.class);
@@ -40,5 +49,28 @@ public class TestBase {
     public void logTestStop(Method m){
         logger.info("Stop test "+ m.getName());}
 
+
+
+    public void verifyGroupListInUI() {
+        if (Boolean.getBoolean("verifyUI")){
+
+        Groups dbGroups =app.db().groups();
+        Groups uiGroups =app.group().all();
+        assertThat(uiGroups, equalTo(dbGroups.stream()
+                .map((g)-> new GroupData().withId((g.getId())).withName(g.getName()))
+                .collect(Collectors.toSet())));}
+
     }
+
+    public void verifyContactListInUI() {
+        if (Boolean.getBoolean("verifyUI")){
+
+            Contacts dbContacts =app.db().contacts();
+            Contacts uiContactcs=app.contact().all();
+            assertThat(uiContactcs, equalTo(dbContacts.stream()
+                    .map((c)-> new ContactData().withId((c.getId())).withFirstname((c.getFirstname())).withLastname((c.getLastname())))
+                    .collect(Collectors.toSet())));}
+
+    }
+}
 
