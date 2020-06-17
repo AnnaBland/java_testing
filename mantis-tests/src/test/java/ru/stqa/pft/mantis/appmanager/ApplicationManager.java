f3670f844554f11fc138953a1473827874bdbde6
anna.blandinova <anna.blandinova@gs-labs.ru>
2020-06-17 18:33:12 +0300

    Task 19

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -24,6 +24,7 @@ public class ApplicationManager {
     private MailHelper mailHelper;
     private JamesHelper jamesHelper;
     private DbHelper dbHelper;
+    private SoapHelper soapHelper;
 
 
     public ApplicationManager(String browser)  {
@@ -100,6 +101,13 @@ public class ApplicationManager {
             jamesHelper = new JamesHelper(this);
         }
         return jamesHelper;
+        }
+
+        public SoapHelper soap(){
+        if(soapHelper==null) {
+            soapHelper = new SoapHelper(this);
+        }
+        return soapHelper;
         }
 
     }
