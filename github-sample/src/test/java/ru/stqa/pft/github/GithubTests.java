85434307732bde73215300af03b80e88cb3eeff4
anna.blandinova <anna.blandinova@gs-labs.ru>
2020-06-22 16:44:23 +0300

    Upload Github project

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub(" 4998cb2fb7ec9ac45401d727c18fe1fd65a7bacf");
+        RepoCommits commits = github.repos()
+                .get(new Coordinates.Simple("AnnaBland", "java_testing")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
